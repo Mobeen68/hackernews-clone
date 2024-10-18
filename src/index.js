4d2411d575da75f28a69ee5604da707eb8037347
Mobeen Ejaz <mobeene98@gmail.com>
2024-10-18 16:17:23 +0500

    project completed

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ import {
 import { BrowserRouter } from "react-router-dom";
 import { AUTH_TOKEN } from "./constants";
 import { setContext } from "@apollo/client/link/context";
+import { split } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 // 2
 const httpLink = createHttpLink({
@@ -29,9 +32,28 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
